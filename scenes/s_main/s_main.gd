aeef4564dab1f5ea8f9128d588aeb70823ae1f12
Kirill-Agievich <k.agievich@hotmail.com>
2021-02-28 13:59:27 +0500

    1

---
+++ scenes/s_main/s_main.gd
@@ -0,0 +1,24 @@
+extends Spatial
+
+#-----------------SCENE--SCRIPT------------------#
+#    Close your game faster by clicking 'Esc'    #
+#   Change mouse mode by clicking 'Shift + F1'   #
+#------------------------------------------------#
+var player = null
+
+
+##################################################
+
+func _ready():
+	OS.center_window()
+	G.pause(0)
+		
+func pause (s=0):
+	get_tree().paused = bool(s)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED if !s else Input.MOUSE_MODE_VISIBLE)	
+
+func _process(delta):
+	if Input.is_action_just_pressed("pause"):
+		G.pause(0)
+	G.sub('menu_pause')
+	
---
+++ дверь.gd
@@ -0,0 +1,18 @@
+extends MeshInstance
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+#var angle = 0
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+#	$Door.rotation.y = angle # Replace with function body.
+
+
+#func open_close():# Called every frame. 'delta' is the elapsed time since the previous fram
+#if !angle:
+	#angle = deg2rad(95)
+#else:
+	#angle = 0

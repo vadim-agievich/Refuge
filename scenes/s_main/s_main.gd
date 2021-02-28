bb1ad70d74f8cb994210f1b389dbf930609a9599
vadim-agievich <vadim.agievich@gmail.com>
2021-02-28 14:48:14 +0500

    1

--- scenes/s_main/s_main.gd
+++ scenes/s_main/s_main.gd
@@ -6,12 +6,11 @@ extends Spatial
 #------------------------------------------------#
 var player = null
 
-
 ##################################################
 
 func _ready():
 	OS.center_window()
-	G.pause(0)
+	pause(0)
 		
 func pause (s=0):
 	get_tree().paused = bool(s)
@@ -19,6 +18,6 @@ func pause (s=0):
 
 func _process(delta):
 	if Input.is_action_just_pressed("pause"):
-		G.pause(0)
-	G.sub('menu_pause')
+		pause(0)
+	sub('menu_pause')
 	
